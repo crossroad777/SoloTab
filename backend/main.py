2c6981b16aa0dbc3fd84edc694a037d580c929e5
SoloTab User <kotani.tatsuhiro@gmail.com>
2026-08-14 01:31:49 +0900

    Fix CORS: allow any localhost port

--- backend/main.py
+++ backend/main.py
@@ -100,7 +100,7 @@ app.add_middleware(
         "http://localhost:8001",
         "http://127.0.0.1:5174",
     ],
-    allow_origin_regex=r"https://.*\.trycloudflare\.com|http://192\.168\.\d+\.\d+:\d+|http://10\.\d+\.\d+\.\d+:\d+",
+    allow_origin_regex=r"https://.*\.trycloudflare\.com|http://192\.168\.\d+\.\d+:\d+|http://10\.\d+\.\d+\.\d+:\d+|http://localhost:\d+|http://127\.0\.0\.1:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
